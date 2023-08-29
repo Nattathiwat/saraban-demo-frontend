4fd4f229b90230ca979cd7151f7dc94b6d241dbf
Thanaporn <thanaporn@softberry.co.th>
2023-08-29 11:14:07 +0700

    fixed bug

--- src/view/my-work/booking-receive/detail.vue
+++ src/view/my-work/booking-receive/detail.vue
@@ -1117,7 +1117,7 @@ export default {
       const request5 = this.axios.get('/master-data/process-type')
       const request6 = this.axios.get('/master-data/permission-type')
       const request7 = this.axios.get('/master-data/department-user?type=1')
-      const request8 = this.axios.get('/bookmethod')
+      const request8 = this.axios.get('/master-data/bookmethod')
       const request10 = this.axios.get(`/filetype?keyword=&page_size=50&page=1`)
 
       this.axios.all([request1, request2, request3, request4, request5, request6, request7, request8, request10])
